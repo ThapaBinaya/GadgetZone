35614dbd2eefbea8294f06e98f876081da005a84
ThapaBinaya <satik.thapa@gmail.com>
2023-07-15 20:22:42 +0545

    first commit

---
+++ resources/views/Reusable_components/user/footer.blade.php
@@ -0,0 +1,140 @@
+<!-- Footer -->
+<footer id="footer" class="page-footer font-small  pt-4" style="background:#0a2929;">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">GADGETZONE</h6>
+        <p style="text-align: justify;">GadgetZone is an online IT accessories website that offers a diverse 
+          range of high-quality products at competitive prices. With a focus on customer satisfaction, 
+          GadgetZone ensures secure payments, fast shipping, responsive customer support, and a 
+          customer-friendly return and refund policy.</p>
+       
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+        <p>
+          <a href="{{url('Shop/Laptop')}}" target="_blank">Laptop</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Phone')}}" target="_blank">Phone</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Printer')}}" target="_blank">Printer</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Moniter')}}" target="_blank"> Moniter  </a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <p>
+          <a href="{{url('about')}}" target="_blank">About Us</a>
+        </p>
+       
+        <p>
+          <a href="{{url('Help')}}" target="_blank">Help</a>
+        </p>
+        <p>
+          <a href="{{url('register')}}" target="_blank">Signup</a>
+        </p>
+          <p>
+          <a href="{{url('login')}}" target="_blank">Login</a>
+        </p>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i>  Kavre, Banepa - 45210</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> gadgetzone2023@gmail.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> Binaya : +977 9876543210</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> Sudip  : +977 9876543211</p>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2021 Copyright:
+          <a href="#">
+            <strong>gadgetzone.com</strong>
+          </a>
+          <br>
+          <span> Developed by <a href="#" target="_blank" style=" font-family: 'Balsamiq Sans', cursive;"><u>Binaya - Sudip</u></a> </span>
+        </p>
+       
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li  class="list-inline-item">
+              <a href="https://twitter.com" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.instagram.com/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
